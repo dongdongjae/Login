1eb87fa33932e911a03136ef1dc0d3cce5ee9873
unknown <32201773@dankook.ac.kr>
2023-11-13 11:19:01 +0900

    hash password

--- routes/users.py
+++ routes/users.py
@@ -1,6 +1,7 @@
 import sqlite3
 
 from fastapi import APIRouter, HTTPException
+from depends.hash_user import check_password, hash_password
 
 from models.user_schema import User, UserSignIn, UserEmail, Username
 
@@ -54,8 +55,11 @@ async def sign_new_user(data: User) -> dict:
     user_email = data.email
     user_password = data.password
     user_username = data.username
+    hashed_password = hash_password(user_password)    
+    
+    
     cur.execute("INSERT INTO user_list (EMAIL, PASSWORD, USERNAME) VALUES(?, ?, ?)",
-                (user_email, user_password, user_username))
+                (user_email, hashed_password, user_username))
     con.commit()
 
     return {
@@ -71,6 +75,9 @@ async def sign_user_in(user: UserSignIn) -> dict:
     cur.execute('''SELECT * FROM user_list WHERE EMAIL=?''', (user_email,))
 
     find_user = cur.fetchall()
+    find_user_password = find_user[0][2]
+    find_username = find_user[0][3]
+
 
     if not find_user:
         raise HTTPException(
@@ -78,14 +85,13 @@ async def sign_user_in(user: UserSignIn) -> dict:
             detail="이메일을 다시 확인해주세요."
         )
 
-    find_user_password = find_user[0][2]
 
-    if user_password != find_user_password:
+    if check_password(input_password=user_password, hashed_password=find_user_password):
+       return {"message": "로그인이 완료되었습니다.", "username" : find_username}
+   
+    else:
         raise HTTPException(
             status_code=404,
             detail="비밀번호를 다시 확인해주세요."
-        )
-
-    return {
-        "message": "로그인이 완료되었습니다."
-    }
+            ) 
+        
